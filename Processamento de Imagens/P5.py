7c93b802cce64d40f7d918e40f66b211dcf3d697
Henrique Falconi <45717665+Henriquefalconi@users.noreply.github.com>
2019-10-05 15:13:50 -0300

    Update P5.py

--- Processamento de Imagens/P5.py
+++ Processamento de Imagens/P5.py
@@ -6,6 +6,7 @@ rows, cols = img.shape
 
 M = np.float32([[1,0,200],[0,1,100]])
 dst = cv2.warpAffine(img,M,(cols,rows))
+res = cv2.resize(img, None, fx=0.2, fy=0.2, interpolation=cv2.INTER_CUBIC)
 
 cv2.imshow('img',dst)
 cv2.waitKey(0)
