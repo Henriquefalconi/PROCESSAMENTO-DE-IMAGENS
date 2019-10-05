e0dcfe158445e71aea66e248c18e7c40e03bafa8
Henrique Falconi <45717665+Henriquefalconi@users.noreply.github.com>
2019-10-05 15:08:38 -0300

    Update P8.py

--- Processamento de Imagens/P8.py
+++ Processamento de Imagens/P8.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 img = cv2.imread('MULHERES.jpg')
 rows,cols,ch = img.shape
 
-pts1 = np.float32([[50,80],[200,70],[20,350],[111,390]])
-pts2 = np.float32([[20,40],[500,20],[500,80],[300,100]])
+pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
 
 M = cv2.getPerspectiveTransform(pts1,pts2)
 
-dst = cv2.warpPerspective(img,M,(400,450))
+dst = cv2.warpPerspective(img,M,(1000,1000))
 
 plt.subplot(121),plt.imshow(img),plt.title('Antes')
 plt.subplot(122),plt.imshow(dst),plt.title('Depois')
