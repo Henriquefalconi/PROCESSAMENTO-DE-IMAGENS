efaf00830802cbc389765a60a3a5e99d8afada0f
Henrique Falconi <45717665+Henriquefalconi@users.noreply.github.com>
2019-10-05 15:12:14 -0300

    Update P7.py

--- Processamento de Imagens/P7.py
+++ Processamento de Imagens/P7.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 img = cv2.imread('MULHERES.jpg')
 rows,cols,ch = img.shape
 
-pts1 = np.float32([[10,50],[20,70],[100,200]])
-pts2 = np.float32([[10,100],[150,50],[100,300]])
+pts1 = np.float32([[50,50],[200,50],[50,200]])
+pts2 = np.float32([[10,100],[200,50],[100,250]])
 
 M = cv2.getAffineTransform(pts1,pts2)
 
